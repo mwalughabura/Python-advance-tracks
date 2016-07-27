c36a9e52fe6f563a7b47d6f7241526c3b63f9d38
mwalugha <mwalughabura@gmail.com>
2016-07-27 14:55:32 +0300

    Managed to finish the second task which was to have the multiples of 4 and 5 and have the sum of them

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,17 @@
 def multiple_sum():
-	#TODO: Your code goes here
-	return
-	
+	listOfNumbers = []
+	addition = 0
+
+	number = 0
+	while number < 1000:
+		if number % 3 == 0:
+			listOfNumbers.append(number)
+		elif number % 5 == 0:
+			listOfNumbers.append(number)
+
+		number+=1
+
+	for x in listOfNumbers:
+		addition+=x
+
+	return addition
