eddd02e87f113786bc7a29d85e33b25d609305cd
mwalugha <mwalughabura@gmail.com>
2016-08-17 11:36:45 +0300

    Changed the file hangman.py to make it work in a series.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -74,22 +74,26 @@ def print_dashes(myword):
 
 
 def prompt_and_check_input():
+	# attempts = 8
+	# while attempts >= 0:
+	# 	attempts-=1
 	print "Hey buddy guess a letter in the word"
 	answer = raw_input(prompt)
 	print answer
 	dashes = print_dashes(myword)
 	myWordInList = list(myword)
+
 	if answer in myWordInList:
 		listDashes = list("_" * len(myword))
 		i = len(myWordInList) - 1
 		while i >= 0:
 			if myWordInList[i] == answer:
 				listDashes[i] = answer
-				print listDashes[i]
-				print i
 			i-=1
-
 		print "".join(listDashes)
+
+	else:
+		print "You have lost"
 
 
 print_dashes(myword)
