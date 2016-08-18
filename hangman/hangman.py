c8f00609a7626ffac1da6536962a5691f7ebba64
mwalugha <mwalughabura@gmail.com>
2016-08-18 12:34:55 +0300

    Managed to learn about making things happen in a sequence and make the hangman work with a list created locally

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,100 +1,120 @@
 import random
 prompt = ">> "
-hangman = {
-
-	"full":"""
-	  ______
-	 |      |
-	 |      0
-	 |     /|\
-	 |     / \
-	 |
-	""",
-	"poll":"""
-	  ______
-	 |
-	 |
-	 |
-	 |
-	 |
-	""",
-	"rope":"""
-	  ______
-	 |      |
-	 |
-	 |
-	 |
-	 |
-	""",
-	"head":"""
-	  ______
-	 |      |
-	 |		0
-	 |
-	 |
-	 |
-	""",
-	"frontLimbs":"""
-	  ______
-	 |      |
-	 |		0
-	 |     / \
-	 |
-	 |
-	""",
-	"body":"""
-	  ______
-	 |      |
-	 |		0
-	 |     /|\
-	 |
-	 |
-	""",
-	"start":"""
-	  
-	 |
-	 |
-	 |
-	 |
-	 |
-	"""
-}
+poll = """
+  ______
+ |
+ |
+ |
+ |
+ |
+"""
+rope = """
+  ______
+ |      |
+ |
+ |
+ |
+ |
+"""
+head = """
+  ______
+ |      |
+ |      0
+ |
+ |
+ |
+"""
+frontLimbs = """
+  ______
+ |      |
+ |      0
+ |     / \\
+ |
+ |
+"""
+body= """
+  ______
+ |      |
+ |      0
+ |     /|\\
+ |
+ |
+"""
+full = """ 
+ ______
+|      |      
+|      0 
+|     /|\\ 
+|     / \\ 
+|
+"""
 
 def choose_random_word():
-	words = ["the", "quick", "brown", "fox", "jumped","over","lazy","dog"]
+	words = 
 	return  random.choice(words)
 
-myword = choose_random_word()
+#myword = choose_random_word()
 
 def print_dashes(myword):
 	print myword
-	dashes = "_ " * len(myword)
+	dashes = "_" * len(myword)
 	print dashes
 	return dashes
 
+def play_again():
+	play = raw_input("Do you want to play again?\n")
+	if play == "no":
+		print "see you next time"
+	elif play == "yes":
+		pass
+	else:
+		play = raw_input("Enter either 'yes' or 'no'.\n")
+	return play
+
 
 def prompt_and_check_input():
-	# attempts = 8
-	# while attempts >= 0:
-	# 	attempts-=1
-	print "Hey buddy guess a letter in the word"
-	answer = raw_input(prompt)
-	print answer
-	dashes = print_dashes(myword)
-	myWordInList = list(myword)
-
-	if answer in myWordInList:
+#answer = None
+	play = "yes"
+	while play == "yes":
+		myword = choose_random_word()
+		dashes = print_dashes(myword)
+		myWordInList = list(myword)
 		listDashes = list("_" * len(myword))
-		i = len(myWordInList) - 1
-		while i >= 0:
-			if myWordInList[i] == answer:
-				listDashes[i] = answer
-			i-=1
-		print "".join(listDashes)
+		trials = 0
+		print_dashes(myword)
+		while trials <= 5:
+			print "Hey buddy guess a letter in the word"
+			answer = raw_input(prompt)
 
-	else:
-		print "You have lost"
+			if answer in myWordInList:
+				i = len(myWordInList) - 1
+				while i >= 0:
+					if myWordInList[i] == answer:
+						listDashes[i] = answer
+					i-=1
+				print "".join(listDashes)
+				if listDashes == myWordInList:
+					print "That was great"
+					play = play_again()
+					trials = 6
+
+			else:
+				if trials == 0:
+					print poll
+				elif trials == 1:
+					print rope
+				elif trials == 2:
+					print head
+				elif trials == 3:
+					print frontLimbs
+				elif trials == 4:
+					print body
+				else:
+					print "Try again"
+					print full
+					play = play_again()
+				trials += 1
 
 
-print_dashes(myword)
-prompt_and_check_input()
+if __name__ == "__main__":
+	prompt_and_check_input()
